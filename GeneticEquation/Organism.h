9cf52568da23c9fe130add051dec93219a62a6b9
Michał Narajowski <michal.narajowski@gmail.com>
2015-01-25 13:43:00 +0100

    passing CNodeFactory to COrganism in constructor

--- GeneticEquation/Organism.h
+++ GeneticEquation/Organism.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "Problem.h"
 #include "Node.h"
+#include "NodeFactory.h"
 
 class COrganism
 {
 	CProblem cProblem;
 	CNode* cRoot;
+	CNodeFactory* cFactory;
 public:
-	COrganism(CProblem cProblem);
+	COrganism(CNodeFactory* cFactory, CProblem cProblem);
 	~COrganism();
 	std::string sToString();
 	void vCollapse();
