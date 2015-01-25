f253c6e38a042a1eb8812720c1e3ad387dd69977
Michał Narajowski <michal.narajowski@gmail.com>
2015-01-25 13:32:21 +0100

    NodeFactory class

---
+++ GeneticEquation/NodeFactory.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Random.h"
+#include "Op.h"
+#include "MathVariable.h"
+#include "MathValue.h"
+
+class CNodeFactory	
+{
+	CRandom c_random;
+	std::vector<COp> v_operators;
+	std::vector<CMathVariable> v_vars;
+	std::vector<CMathValue> v_values;
+	int i_number_of_vars;
+public:
+	CNodeFactory(int i_number_of_vars);
+	~CNodeFactory();
+
+	CMathVariable* cGetRandomVariable();
+	CMathValue* cGetRandomValue();
+	COp* cGetRandomOperator();
+private:
+	void vPopulateOperators();
+	void vPopulateVariables();
+	void vPopulateValues();
+};
+
