92c701041fa077c76a6c078a24969044bdc43a61
Michał Narajowski <michal.narajowski@gmail.com>
2015-01-25 15:51:29 +0100

    Fixed vector of nodes to vector of pointers to nodes

--- GeneticEquation/NodeFactory.cpp
+++ GeneticEquation/NodeFactory.cpp
@@ -13,6 +13,8 @@ CNodeFactory::CNodeFactory(int i_number_of_vars) : i_number_of_vars(i_number_of_
 CNodeFactory::~CNodeFactory()
 {
 	delete c_random;
+	for (int i = 0; i < v_nodes.size(); i++)
+		delete v_nodes[i];
 }
 
 CMathVariable* CNodeFactory::cGetRandomVariable()
@@ -32,7 +34,7 @@ COp* CNodeFactory::cGetRandomOperator()
 
 CNode* CNodeFactory::cGetRandomNode()
 {
-	return v_nodes[c_random->iNextInt(v_operators.size() - 1)].clone();
+	return v_nodes[c_random->iNextInt(v_operators.size() - 1)]->clone();
 }
 
 void CNodeFactory::vPopulateOperators()
@@ -46,12 +48,12 @@ void CNodeFactory::vPopulateVariables()
 	for (int i = 0; i < i_number_of_vars; i++)
 	{
 		v_vars.push_back(CMathVariable(nullptr, std::string(1, names[names.size() - i_number_of_vars + i])));
-		v_nodes.push_back(CMathVariable(nullptr, std::string(1, names[names.size() - i_number_of_vars + i])));
+		v_nodes.push_back(new CMathVariable(nullptr, std::string(1, names[names.size() - i_number_of_vars + i])));
 	}
 }
 
 void CNodeFactory::vPopulateValues()
 {
 	v_values.push_back(CMathValue(nullptr));
-	v_nodes.push_back(CMathValue(nullptr));
+	v_nodes.push_back(new CMathValue(nullptr));
 }
--- GeneticEquation/NodeFactory.h
+++ GeneticEquation/NodeFactory.h
@@ -10,7 +10,7 @@ class CNodeFactory
 	std::vector<COp> v_operators;
 	std::vector<CMathVariable> v_vars;
 	std::vector<CMathValue> v_values;
-	std::vector<CNode> v_nodes;
+	std::vector<CNode*> v_nodes;
 	int i_number_of_vars;
 public:
 	CNodeFactory(int i_number_of_vars);
